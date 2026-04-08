eeddd6793c33afef234b336b87a9ac8b98fe26f9
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-08 11:07:55 +0500

    small changes :)

--- lab1_task1.py
+++ lab1_task1.py
@@ -2,6 +2,9 @@
 # rev_num="".join(reversed(num))
 # print("Number in reverse is:",rev_num)
 
+
+# I used the above logic earlier when you gave us the tasks in online class, below is the logic using lists as we learned today:
+
 list=[]
 num=input("Enter any number:")
 for i in num:
