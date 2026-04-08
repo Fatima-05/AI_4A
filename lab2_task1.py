e02616260a86426d5ce842483860cd760bbabdad
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-08 10:27:58 +0500

    lab 2 task 1

---
+++ lab2_task1.py
@@ -0,0 +1,17 @@
+list1=[]
+print("Enter values of first list:")
+for i in range(5):
+    val=input("Enter value:")
+    n=int(val)
+    list1.append(n)
+
+list2=[]
+print("Enter values of second list:")
+for i in range(5):
+    val=input("Enter value:")
+    n=int(val)
+    list2.append(n)
+
+list3=list1+list2
+list3.sort()
+print(list3)
