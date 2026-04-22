7e2b9bd7412f6455e3bd83e9ab82a49215cea954
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-22 10:08:20 +0500

    dfs lab task 3

---
+++ dfs_task3.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+
+
+def get_path(node):
+    path = []
+    while node:
+        path.append(node.state)
+        node = node.parent
+    return path[::-1]
+
+
+def BFS(initial_state, goal_test, get_children):
+    frontier = [Node(initial_state)]
+    explored = set()
+
+    while frontier:
+        node = frontier.pop(len(frontier)-1)
+
+        if goal_test(node.state):
+            return get_path(node)
+
+        explored.add(node.state)
+
+        for child_state in get_children(node.state):
+            if child_state not in explored and all(n.state != child_state for n in frontier):
+                frontier.append(Node(child_state, node))
+
+    return None
+
+
+def goal_test(state):
+    return state=='L'
+
+
+def get_children(state):
+    return graph[state]
+
+
+if __name__ == "__main__":
+    graph = {
+        'S':['A', 'B'],
+        'A':['S', 'C'],
+        'B':['S'],
+        'C':['A', 'D'],
+        'D':['C','E', 'L'],
+        'E':['D'],
+        'L':['D'],
+    }
+
+    result=BFS('S', goal_test, get_children)
+    print(result)
