f915c09410d15496ee2da026639796dd099d7f9f
Fatima-05 <fatima.tu.zahra.005@gmail.com>
2026-04-28 16:08:40 +0500

    final

--- dfs_task1.py
+++ dfs_task1.py
@@ -17,24 +17,24 @@ def dfs_8puzzle():
     frontier=[start]
     explored=set()
     while frontier:
-        cur=frontier.pop() 
-        if cur in explored:
+        current=frontier.pop() 
+        if current in explored:
             continue
-        explored.add(cur)
-        if cur==goal:
-            return get_path(graph,cur)
+        explored.add(current)
+        if current==goal:
+            return get_path(graph,current)
         for i in range(3):
             for j in range(3):
-                if cur[i][j]==0:
+                if current[i][j]==0:
                     x,y=i,j
         for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
             nx,ny=x+dx,y+dy
             if 0<=nx<3 and 0<=ny<3:
-                new_state=[list(row) for row in cur]
+                new_state=[list(row) for row in current]
                 new_state[x][y], new_state[nx][ny]=new_state[nx][ny],new_state[x][y]
                 new_tuple=tuple(tuple(row) for row in new_state)
                 if new_tuple not in explored:
-                    graph[new_tuple]=Node(new_tuple,cur)
+                    graph[new_tuple]=Node(new_tuple,current)
                     frontier.append(new_tuple)   
     return None
 
